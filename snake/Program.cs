30078876949c06024924de8e7ea7e4ac16731998
Offbeatsofa <tuckerdrown77@gmail.com>
2025-05-09 06:26:43 +0000

    initial commit

---
+++ snake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Snake
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Snake! Press Enter to start.");
+            Console.ReadLine();
+        }
+
+        static int[] Spawnpoints()
+        {
+            int width = Console.WindowWidth;
+            int height = Console.WindowHeight;
+            var rand = new Random();
+            int snakeSpawnX = rand.Next(0, width);
+            int snakeSpawnY = rand.Next(0, height);
+            int appleSpawnX = rand.Next(0, width);
+            int appleSpawnY = rand.Next(0, height);
+            return new int[] { snakeSpawnX, snakeSpawnY, appleSpawnX, appleSpawnY };
+        }
+        
+        
+    }
+    
+}
